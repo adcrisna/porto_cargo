e7aac6788e331b624d43ad39c40df0ede3f0b0bb
Ade Crisna <adecrisna977@gmail.com>
2023-11-10 16:58:44 +0700

    pdf policy summary

--- resources/views/pdf/policy_summary.blade.php
+++ resources/views/pdf/policy_summary.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Premium Note</title>
+    <title>Policy Summary</title>
     <style account="text/css">
         @font-face {
             font-family: SourceSansPro;
@@ -14,6 +14,19 @@
             float: left;
             height: 30px;
         }
+
+        .titlePolicy {
+            margin-top: 80px;
+        }
+
+        .textTitle {
+            align-content: center;
+            height: 35px;
+            font-size: 12px;
+
+        }
+
+        .detailPolicy {}
     </style>
 </head>
 
@@ -23,7 +36,220 @@
             <img src='{{ public_path() . '/images/salvus-logo.png' }}' class="logoSalvus">
         </header>
         <main>
-
+            <div class="titlePolicy">
+                <table>
+                    <tbody>
+                        <tr>
+                            <td style="width: 550px">
+                                <h2 style="color: #2156A5; margin :0px !important; padding: 0px !important">Ringkasan
+                                    Polis</h2>
+                            </td>
+                            <td>
+                                <p style="margin :0px !important; padding: 0px !important"><i>Policy Summary</i></p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <hr style="color: black">
+            <div class="textTitle">
+                <table style="background-color: #2156A5; font-size: 12px">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <p style="color: white; padding: 1px;  margin: 1px;">INFORMASI PEMEGANG RINGKASAN POLIS
+                                </p>
+                            </td>
+                            <td style="width: 200px"></td>
+                            <td>
+                                <p style="color: white; padding: 1px;  margin: 1px;"><i>POLICY SUMMARY HOLDER
+                                        INFORMATION</i></p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <center>
+                <p style="color: #2156A5; font-size: 14px; margin :0px !important; padding: 0px !important"><b>Cargo
+                        Insurance PT. Struktur Pintar</b></p>
+            </center>
+            <div class="detailPolicy">
+                <table>
+                    <tbody>
+                        <tr>
+                            <td style="width: 310px">
+                                <p style="color: #2156A5; float: right; font-size: 14px;">Nomor Ringkasan Polis <br>
+                                    <i style="font-size: 12px !important">Policy Summary
+                                        Number</i>
+                                </p>
+                            </td>
+                            <td style="margin-top: 25px; margin-left: 20px; width: 20px"> : </td>
+                            <td>
+                                <p style="margin-top: 18px; font-size: 14px;">7258874</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 310px">
+                                <p
+                                    style="color: #2156A5; float: right; font-size: 14px; margin :0px !important; padding: 0px !important">
+                                    Nama Asuransi <br> <i style="font-size: 12px !important">Insurer
+                                        Name
+                                    </i></p>
+                            </td>
+                            <td
+                                style="margin-top: 25px; margin-left: 20px; width: 20px; margin :0px !important; padding: 0px !important">
+                                : </td>
+                            <td>
+                                <p
+                                    style="margin-top: 18px; font-size: 14px; margin :0px !important; padding: 0px !important">
+                                    PT. Malacca Trust Wuwungan Insurance Tbk
+                                </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 310px">
+                                <p
+                                    style="color: #2156A5; float: right; font-size: 14px; margin :0px !important; padding: 0px !important">
+                                    Alamat Tertanggung <br>
+                                    <i style="font-size: 12px !important">Address of Insured
+                                    </i>
+                                </p>
+                            </td>
+                            <td
+                                style="margin-top: 25px; margin-left: 20px; width: 20px; margin :0px !important; padding: 0px !important">
+                                : </td>
+                            <td>
+                                <p
+                                    style="margin-top: 18px; font-size: 14px; margin :0px !important; padding: 0px !important">
+                                    Jl. Rawa Sumur 2, Blok BB No. 8A-8B,
+                                    Kawasan Industri
+                                    Pulo Gadung, Jakarta Timur</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 310px">
+                                <p style="color: #2156A5; float: right; font-size: 14px;">Periode Polis <br> <i
+                                        style="font-size: 12px !important">Policy
+                                        Period </i><br>
+                                    (DD/MM/YYYY)</p>
+                            </td>
+                            <td style="margin-top: 25px; margin-left: 20px; width: 20px"> : </td>
+                            <td>
+                                <p style="margin-top: 18px; font-size: 14px;">25-06-2023 - 26-06-2023</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 310px">
+                                <p style="color: #2156A5; float: right; font-size: 14px;">Periode
+                                    Diterbitkan<br><i style="font-size: 12px !important">Policy Period
+                                    </i><br>
+                                    (DD/MM/YYYY)</p>
+                            </td>
+                            <td style="margin-top: 25px; margin-left: 20px; width: 20px"> : </td>
+                            <td>
+                                <p style="margin-top: 18px; font-size: 14px;">25/6/2023</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 310px">
+                                <p style="color: #2156A5; float: right; font-size: 14px;">Jaminan
+                                    Asuransi<br><i style="font-size: 12px !important">Coverage Insurance
+                                    </i></p>
+                            </td>
+                            <td style="margin-top: 25px; margin-left: 20px; width: 20px"> : </td>
+                            <td>
+                                <p style="margin-top: 18px; font-size: 14px;">Institute Cargo Clause "A"</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 310px">
+                                <p
+                                    style="color: #2156A5; float: right; font-size: 14px; margin :0px !important; padding: 0px !important">
+                                    Perjalanan<br><i style="font-size: 12px !important">Voyage
+                                    </i></p>
+                            </td>
+                            <td
+                                style="margin-top: 25px; margin-left: 20px; width: 20px; margin :0px !important; padding: 0px !important">
+                                : </td>
+                            <td>
+                                <p
+                                    style="margin-top: 18px; font-size: 14px; margin :0px !important; padding: 0px !important">
+                                    From Jakarta, Indonesia to Surabaya,
+                                    Indonesia <br> From Surabaya, Indonesia to Angansa, Kalimantan, Indonesia</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 310px">
+                                <p
+                                    style="color: #2156A5; float: right; font-size: 14px; margin :0px !important; padding: 0px !important">
+                                    Barang yang
+                                    Diasuransikan<br><i style="font-size: 12px !important">Interest Insured
+                                    </i></p>
+                            </td>
+                            <td
+                                style="margin-top: 25px; margin-left: 20px; width: 20px; margin :0px !important; padding: 0px !important">
+                                : </td>
+                            <td>
+                                <p
+                                    style="margin-top: 18px; font-size: 14px; margin :0px !important; padding: 0px !important">
+                                    Set of Grouting Compound "Standart" for
+                                    the Installation of Lineas WIM Sensors</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 310px">
+                                <p
+                                    style="color: #2156A5; float: right; font-size: 14px; margin :0px !important; padding: 0px !important">
+                                    Total Sum Insured<br></p>
+                            </td>
+                            <td
+                                style="margin-top: 25px; margin-left: 20px; width: 20px; margin :0px !important; padding: 0px !important">
+                                : </td>
+                            <td>
+                                <p
+                                    style="margin-top: 18px; font-size: 14px; margin :0px !important; padding: 0px !important">
+                                    IDR 1.268.596.510</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 310px">
+                                <p style="color: #2156A5; float: right; font-size: 14px; ">Perhitungan Premi<br><i
+                                        style="font-size: 12px !important">Premi
+                                        Calculation
+                                    </i></p>
+                            </td>
+                            <td style="margin-top: 25px; margin-left: 20px; width: 20px"> : </td>
+                            <td>
+                                <p style="margin-top: 18px; font-size: 14px;">IDR 1.268.596.510 x 0.1% = IDR 1.268.530
+                                </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 310px">
+                                <p style="color: #2156A5; float: right; font-size: 14px;">Kendaraan<br><i
+                                        style="font-size: 12px !important">Conveyance
+                                    </i></p>
+                            </td>
+                            <td style="margin-top: 25px; margin-left: 20px; width: 20px"> : </td>
+                            <td>
+                                <p style="margin-top: 18px; font-size: 14px;">By land (truck) and sea vessel conveyance
+                                </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 310px">
+                                <p style="color: #2156A5; float: right; font-size: 14px;">Premi & Biaya
+                                    Polis<br><i style="font-size: 12px !important">Premium & Policy Cost
+                                    </i></p>
+                            </td>
+                            <td style="margin-top: 25px; margin-left: 20px; width: 20px"> : </td>
+                            <td>
+                                <p style="margin-top: 18px; font-size: 14px;">IDR 1.268.530 + Rp 60.000</p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </main>
     </div>
 </body>
